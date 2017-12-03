7264595469242bc072bb74de06847272f330d07b
David Cumps <david@cumps.be>
2017-12-03 23:26:45 +0100

    give packer nomad consul vault a try

---
+++ nomad-consul-vault/eu-west-1/terraform.tfvars
@@ -0,0 +1,7 @@
+region            = "eu-west-1"
+ami               = "ami-6ce26316"
+instance_type     = "t2.medium"
+key_name          = "KEY_NAME"
+server_count      = "3"
+client_count      = "4"
+
---
+++ nomad-consul-vault/scripts/server.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -e
+
+CONFIGDIR=/ops/config
+
+CONSULCONFIGDIR=/etc/consul.d
+VAULTCONFIGDIR=/etc/vault.d
+NOMADCONFIGDIR=/etc/nomad.d
+HOME_DIR=ubuntu
+
+# Wait for network
+sleep 15
+
+# IP_ADDRESS=$(curl http://instance-data/latest/meta-data/local-ipv4)
+IP_ADDRESS="$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"
+DOCKER_BRIDGE_IP_ADDRESS=(`ifconfig docker0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`)
+CLOUD=$1
+SERVER_COUNT=$2
+RETRY_JOIN=$3
+
+# Consul
+sed -i "s/IP_ADDRESS/$IP_ADDRESS/g" $CONFIGDIR/consul.json
+sed -i "s/SERVER_COUNT/$SERVER_COUNT/g" $CONFIGDIR/consul.json
+sed -i "s/RETRY_JOIN/$RETRY_JOIN/g" $CONFIGDIR/consul.json
+sudo cp $CONFIGDIR/consul.json $CONSULCONFIGDIR
+sudo cp $CONFIGDIR/consul_$CLOUD.service /etc/systemd/system/consul.service
+
+sudo systemctl start consul.service
+sleep 10
+export CONSUL_HTTP_ADDR=$IP_ADDRESS:8500
+export CONSUL_RPC_ADDR=$IP_ADDRESS:8400
+
+# Vault
+sed -i "s/IP_ADDRESS/$IP_ADDRESS/g" $CONFIGDIR/vault.hcl
+sudo cp $CONFIGDIR/vault.hcl $VAULTCONFIGDIR
+sudo cp $CONFIGDIR/vault.service /etc/systemd/system/vault.service
+
+sudo systemctl start vault.service
+
+# Nomad
+sed -i "s/SERVER_COUNT/$SERVER_COUNT/g" $CONFIGDIR/nomad.hcl
+sudo cp $CONFIGDIR/nomad.hcl $NOMADCONFIGDIR
+sudo cp $CONFIGDIR/nomad.service /etc/systemd/system/nomad.service
+
+sudo systemctl start nomad.service
+sleep 10
+export NOMAD_ADDR=http://$IP_ADDRESS:4646
+
+# Add hostname to /etc/hosts
+echo "127.0.0.1 $(hostname)" | sudo tee --append /etc/hosts
+
+# Add Docker bridge network IP to /etc/resolv.conf (at the top)
+echo "nameserver $DOCKER_BRIDGE_IP_ADDRESS" | sudo tee /etc/resolv.conf.new
+cat /etc/resolv.conf | sudo tee --append /etc/resolv.conf.new
+sudo mv /etc/resolv.conf.new /etc/resolv.conf
+
+# Set env vars for tool CLIs
+echo "export CONSUL_RPC_ADDR=$IP_ADDRESS:8400" | sudo tee --append /home/$HOME_DIR/.bashrc
+echo "export CONSUL_HTTP_ADDR=$IP_ADDRESS:8500" | sudo tee --append /home/$HOME_DIR/.bashrc
+echo "export VAULT_ADDR=http://$IP_ADDRESS:8200" | sudo tee --append /home/$HOME_DIR/.bashrc
+echo "export NOMAD_ADDR=http://$IP_ADDRESS:4646" | sudo tee --append /home/$HOME_DIR/.bashrc
