771f8192fa77ae6ee380569312e9c29466d6e059
David Cumps <david@cumps.be>
2017-12-04 00:10:36 +0100

    no docker yet for ubuntu 17.10

--- nomad-consul-vault/scripts/setup.sh
+++ nomad-consul-vault/scripts/setup.sh
@@ -21,6 +21,9 @@ NOMADDOWNLOAD=https://releases.hashicorp.com/nomad/${NOMADVERSION}/nomad_${NOMAD
 NOMADCONFIGDIR=/etc/nomad.d
 NOMADDIR=/opt/nomad
 
+#$DOCKERREPO=ubuntu-`lsb_release -c | awk '{print $2}'`
+$DOCKERREPO=ubuntu-zesty
+
 # Dependencies
 sudo apt-get install -y software-properties-common
 sudo apt-get update
@@ -72,7 +75,7 @@ sudo mkdir -p $NOMADDIR
 sudo chmod 755 $NOMADDIR
 
 # Docker
-echo deb https://apt.dockerproject.org/repo ubuntu-`lsb_release -c | awk '{print $2}'` main | sudo tee /etc/apt/sources.list.d/docker.list
+echo deb https://apt.dockerproject.org/repo $DOCKERREPO main | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-get update
 sudo apt-get install -y docker-engine
